a3493099f569cae1d0a79ff2c9cb5595e4aaae43
Luzivan Gois <luzivangois@outlook.com>
2024-05-13 22:52:34 -0300

    atualizacao do first commit

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.post("/register")
+# def register(name: str, login: str, password: str, role: str):
